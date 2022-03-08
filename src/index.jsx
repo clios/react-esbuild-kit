a6adcb06ded48801dc6258c4586cc1ce94a2b984
Cliemtor Fabros <cliemtor08@gmail.com>
2022-03-08 09:04:29 +0800

    Update welcome message

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-ReactDOM.render(<h1>Nice, you made it...</h1>, document.getElementById('root'))
+ReactDOM.render(<h1>Nice 👌</h1>, document.getElementById('root'))
